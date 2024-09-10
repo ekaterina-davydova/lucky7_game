591a47bf8e4ab59089406e7d4d9b2d9a361e17ff
Ekaterina Davydova <ekaterina.davydova@student.hamk.fi>
2024-09-10 19:03:26 +0300

    Second step is done
    The money is added to the game and the function which checking of how many sevens appeared

--- src/Assignment.java
+++ src/Assignment.java
@@ -4,29 +4,65 @@ public class Assignment
 {
      public static void main(String[] args) 
      {
- 
+        int playerMoney = 5;
         Random random = new Random();
         
-        int randomNumber1;
-        randomNumber1 = random.nextInt(10) + 1; 
-        System.out.println("Random number between 1-10: " + randomNumber1);
-
-        int randomNumber2;
-        randomNumber2 = random.nextInt(10) + 1; 
-        System.out.println("Random number between 1-10: " + randomNumber2);
-
-        int randomNumber3;
-        randomNumber3 = random.nextInt(10) + 1; 
-        System.out.println("Random number between 1-10: " + randomNumber3);
-
-        if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7)
+        while(playerMoney > 0)
         {
-            System.out.println("You won");
+            playerMoney--;
+            System.out.println("Your money " + playerMoney);
+
+            int randomNumber1;
+            randomNumber1 = random.nextInt(10) + 1; 
+            System.out.println("Random number between 1-10: " + randomNumber1);
+
+            int randomNumber2;
+            randomNumber2 = random.nextInt(10) + 1; 
+            System.out.println("Random number between 1-10: " + randomNumber2);
+
+            int randomNumber3;
+            randomNumber3 = random.nextInt(10) + 1; 
+            System.out.println("Random number between 1-10: " + randomNumber3);
+
+            int numbersOfSeven = 0;
+
+            if (randomNumber1 == 7)
+            {
+                numbersOfSeven++;
+            }
+            if (randomNumber2 == 7)
+            {
+                numbersOfSeven++;
+            }
+            if (randomNumber3 == 7)
+            {
+                numbersOfSeven++;
+            }
+
+            if (numbersOfSeven == 1)
+            {
+                System.out.println("You won 3€");
+                playerMoney = playerMoney + 3;
+            }
+
+            else if (numbersOfSeven == 2)
+            {
+                System.out.println("You won 5€");
+                playerMoney = playerMoney + 5;
+            }
+
+            else if (numbersOfSeven == 3)
+            {
+                System.out.println("You won 10€");
+                playerMoney = playerMoney + 10;
+            }
+
+            else
+            {
+                System.out.println("You lost.");
+            }
         }
-        else
-        {
-            System.out.println("You lost");
-        }
-        
+
+        System.out.println("End of the game. You are out of money ");
     }
 }
